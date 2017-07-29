f69640beea83ecbf3e6e51bc58dffafe1e37116f
Brian Walsh <brian@bwalsh.com>
2017-07-29 13:49:41 -0700

    sort buckets by 'fewest number of unique values'

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -10,9 +10,12 @@ export class Cohort extends Component {
     // create renderable facets
     const facet_items = _.map(this.props.facets, function(value, key, object) {
       const buckets = _.map(value.buckets, function(bucket) {
-        return <li key={bucket.key}>{bucket.key} <span className="badge">{bucket.doc_count}</span></li>;
+        return <li key={bucket.key}>{bucket.key} <span className="badge">{bucket.doc_count ? bucket.doc_count : bucket.value}</span></li>;
       })
-      const other = <li key={'other'}><em>{'other'}</em> <span className="badge">{value.sum_other_doc_count}</span></li> ;
+      var other;
+      if (value.sum_other_doc_count > 0) {
+        other = <li key={'other'}><em>{'other'}</em> <span className="badge">{value.sum_other_doc_count}</span></li> ;
+      }
       return (
         <li key={key}>
             {key}:
@@ -48,10 +51,31 @@ function mapStateToProps(state, own) {
       return key.startsWith(`${own.params.label}.`);
     });
 
+  // sort by bucket's 'remainder', if low or 0 then should be good to query on ...
+  // var my_facets_sorted =  _.chain(my_facets).sortBy(function(facet, key, object) {
+  //   return facet.sum_other_doc_count;
+  // }).value();
+  const sortKeysBy = function (obj, comparator) {
+      var keys = _.sortBy(_.keys(obj), function (key) {
+          return comparator ? comparator(obj[key], key) : key;
+      });
+
+      return _.object(keys, _.map(keys, function (key) {
+          return obj[key];
+      }));
+  }
+
+
+  const my_facets_sorted = sortKeysBy(my_facets, function (facet, key) {
+      return facet.sum_other_doc_count;
+  });
+
+  console.log(my_facets_sorted);
+
   return {
     label: own.params.label,
     schema: state.schema,
-    facets: my_facets,
+    facets: my_facets_sorted,
   }
 }
 export default connect(mapStateToProps) (Cohort)
