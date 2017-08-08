3628cab0f188735d1c7dc585cafe9ccb21c00296
Brian Walsh <brian@bwalsh.com>
2017-08-08 11:12:50 -0700

    react-tap-event-plugin

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,7 +5,11 @@ import { push } from 'react-router-redux'
 import * as _ from 'underscore'
 
 
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import PropTypes from 'prop-types';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
+
 
 export class Table extends Component {
 
@@ -19,25 +23,69 @@ export class Table extends Component {
 
   // render the table
   render() {
-
+    const classes = this.props.classes;
     if (this.props.data) {
-    // map the first item to columns
-    const columns = _.map(this.props.data[0], function(item, key, object) {
-      console.log(key);
+      // map the first item to columns TODO - map from facets
+      const columns = _.map(this.props.data[0], function(item, key, object) {
+        return (
+          <TableCell
+            key={key}>
+              {key}
+          </TableCell>
+        )
+      });
+
+      const rows = _.map(this.props.data, function(item, index, list) {
+        return (
+          <TableRow key={item.gid}>
+            {
+              const rowCells = _.map(item, function(row, key, obj) {
+                return (
+                  <TableCell>
+                    {row[key]}
+                  </TableCell>
+                );
+              });
+            }
+          </TableRow>
+        );
+      }
+      {data.map(n => {
+        return (
+          <TableRow key={n.id}>
+            <TableCell>
+              {n.name}
+            </TableCell>
+            <TableCell numeric>
+              {n.calories}
+            </TableCell>
+            <TableCell numeric>
+              {n.fat}
+            </TableCell>
+            <TableCell numeric>
+              {n.carbs}
+            </TableCell>
+            <TableCell numeric>
+              {n.protein}
+            </TableCell>
+          </TableRow>
+        );
+      })}
+
       return (
-        <TableHeaderColumn
-          key={key}
-          dataField={key} >
-            {key}
-        </TableHeaderColumn>
-      )
-    });
-    // gid is always the key
-    return (
-      <BootstrapTable data={this.props.data} striped hover keyField='gid'>
-        {columns}
-      </BootstrapTable>
-    ) ;
+        <Paper className={this.classes.paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {columns}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows}
+            </TableBody>
+          </Table>
+        </Paper>
+      ) ;
     } else {
       return (
         <div>no results</div>
@@ -49,20 +97,19 @@ export class Table extends Component {
 function mapStateToProps(state, own) {
   console.log('Table mapStateToProps state', state);
   console.log('Table mapStateToProps own', own)
-  const products = [{
-        gid: 1,
-        name: "Product1",
-        price: 120
-    }, {
-        gid: 2,
-        name: "Product2",
-        price: 80
-    }];
-
   const data = state.query.results ? state.query.results.map(function(result) {return {...result.properties, gid: result.gid}}) : []
   return {
-    data: data, //products
+    data: data,
   }
 }
+
+const styleSheet = createStyleSheet(theme => ({
+  paper: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+}));
+
 
 export default connect(mapStateToProps) (Table)
