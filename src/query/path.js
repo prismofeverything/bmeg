5363add8957a9c26abfd4a3ca276dbb8a31838f4
Ryan Spangler <ryan.spangler@gmail.com>
2017-08-10 10:33:41 -0700

    fixed syntax error

--- src/query/path.js
+++ src/query/path.js
@@ -145,7 +145,7 @@ function translateQuery(schema, visited, focus) {
       return _.reduce(journey.slice(1), function(subquery, step) {
         if (step['label']) {
           // it is an edge
-          return (step['from']) ? subquery.incoming(step['label']) : return subquery.outgoing(step['label'])
+          return (step['from']) ? subquery.incoming(step['label']) : subquery.outgoing(step['label'])
         } else {
           // it is a vertex
           return applyFacets(subquery, nodes[step].facets)
