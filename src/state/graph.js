ff7c85f8d3d5a413f3483b385a9205b9cac33baf
Ryan Spangler <ryan.spangler@gmail.com>
2017-09-14 20:42:26 -0700

    removed old code

--- src/state/graph.js
+++ src/state/graph.js
@@ -1,3 +1,5 @@
+// thanks to http://blog.benoitvallon.com/data-structures-in-javascript/the-graph-data-structure/ for the code
+
 export default class Graph {
   constructor() {
     this.vertices = [];
@@ -21,21 +23,21 @@ export default class Graph {
     }
   }
 
-  addEdge(vertex1, vertex2) {
-    this.edges[vertex1].push(vertex2);
-    this.edges[vertex2].push(vertex1);
+  addEdge(a, b) {
+    this.edges[a].push(b);
+    this.edges[b].push(a);
     this.numberOfEdges++;
   }
 
-  removeEdge(vertex1, vertex2) {
-    const index1 = this.edges[vertex1] ? this.edges[vertex1].indexOf(vertex2) : -1;
-    const index2 = this.edges[vertex2] ? this.edges[vertex2].indexOf(vertex1) : -1;
+  removeEdge(a, b) {
+    const index1 = this.edges[a] ? this.edges[a].indexOf(b) : -1;
+    const index2 = this.edges[b] ? this.edges[b].indexOf(a) : -1;
     if(~index1) {
-      this.edges[vertex1].splice(index1, 1);
+      this.edges[a].splice(index1, 1);
       this.numberOfEdges--;
     }
     if(~index2) {
-      this.edges[vertex2].splice(index2, 1);
+      this.edges[b].splice(index2, 1);
     }
   }
 
@@ -127,134 +129,6 @@ export default class Graph {
     }, this).join(' | '));
   }
 }
-
-// export default function Graph() {
-//   this.vertices = [];
-//   this.edges = [];
-//   this.numberOfEdges = 0;
-// }
-
-// Graph.prototype.addVertex = function(vertex) {
-//   this.vertices.push(vertex);
-//   this.edges[vertex] = [];
-// };
-
-// Graph.prototype.removeVertex = function(vertex) {
-//   var index = this.vertices.indexOf(vertex);
-//   if(~index) {
-//     this.vertices.splice(index, 1);
-//   }
-//   while(this.edges[vertex].length) {
-//     var adjacentVertex = this.edges[vertex].pop();
-//     this.removeEdge(adjacentVertex, vertex);
-//   }
-// };
-
-// Graph.prototype.addEdge = function(vertex1, vertex2) {
-//   this.edges[vertex1].push(vertex2);
-//   this.edges[vertex2].push(vertex1);
-//   this.numberOfEdges++;
-// };
-
-// Graph.prototype.removeEdge = function(vertex1, vertex2) {
-//   var index1 = this.edges[vertex1] ? this.edges[vertex1].indexOf(vertex2) : -1;
-//   var index2 = this.edges[vertex2] ? this.edges[vertex2].indexOf(vertex1) : -1;
-//   if(~index1) {
-//     this.edges[vertex1].splice(index1, 1);
-//     this.numberOfEdges--;
-//   }
-//   if(~index2) {
-//     this.edges[vertex2].splice(index2, 1);
-//   }
-// };
-
-// Graph.prototype.size = function() {
-//   return this.vertices.length;
-// };
-
-// Graph.prototype.relations = function() {
-//   return this.numberOfEdges;
-// };
-
-// Graph.prototype.traverseDFS = function(vertex, fn) {
-//   if(!~this.vertices.indexOf(vertex)) {
-//     return console.log('Vertex not found');
-//   }
-//   var visited = [];
-//   this._traverseDFS(vertex, visited, fn);
-// };
-
-// Graph.prototype._traverseDFS = function(vertex, visited, fn) {
-//   visited[vertex] = true;
-//   if(this.edges[vertex] !== undefined) {
-//     fn(vertex);
-//   }
-//   for(var i = 0; i < this.edges[vertex].length; i++) {
-//     if(!visited[this.edges[vertex][i]]) {
-//       this._traverseDFS(this.edges[vertex][i], visited, fn);
-//     }
-//   }
-// };
-
-// Graph.prototype.traverseBFS = function(vertex, fn) {
-//   if(!~this.vertices.indexOf(vertex)) {
-//     return console.log('Vertex not found');
-//   }
-//   var queue = [];
-//   queue.push(vertex);
-//   var visited = [];
-//   visited[vertex] = true;
-
-//   while(queue.length) {
-//     vertex = queue.shift();
-//     fn(vertex);
-//     for(var i = 0; i < this.edges[vertex].length; i++) {
-//       if(!visited[this.edges[vertex][i]]) {
-//         visited[this.edges[vertex][i]] = true;
-//         queue.push(this.edges[vertex][i]);
-//       }
-//     }
-//   }
-// };
-
-// Graph.prototype.pathFromTo = function(vertexSource, vertexDestination) {
-//   if(!~this.vertices.indexOf(vertexSource)) {
-//     return console.log('Vertex not found');
-//   }
-//   var queue = [];
-//   queue.push(vertexSource);
-//   var visited = [];
-//   visited[vertexSource] = true;
-//   var paths = [];
-
-//   while(queue.length) {
-//     var vertex = queue.shift();
-//     for(var i = 0; i < this.edges[vertex].length; i++) {
-//       if(!visited[this.edges[vertex][i]]) {
-//         visited[this.edges[vertex][i]] = true;
-//         queue.push(this.edges[vertex][i]);
-//         // save paths between vertices
-//         paths[this.edges[vertex][i]] = vertex;
-//       }
-//     }
-//   }
-//   if(!visited[vertexDestination]) {
-//     return undefined;
-//   }
-
-//   var path = [];
-//   for(var j = vertexDestination; j != vertexSource; j = paths[j]) {
-//     path.push(j);
-//   }
-//   path.push(j);
-//   return path.reverse().join('-');
-// };
-
-// Graph.prototype.print = function() {
-//   console.log(this.vertices.map(function(vertex) {
-//     return (vertex + ' -> ' + this.edges[vertex].join(', ')).trim();
-//   }, this).join(' | '));
-// };
 
 // var graph = new Graph();
 // graph.addVertex(1);
